64e02749e7462193567fa6b0b75290a260393bf9
HollyT85 <h.e.turner@gmail.com>
2023-09-01 19:34:08 +0000

    added inital routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Container } from 'react-bootstrap';
 import './App.css';
 import NavBar from './components/NavBar';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Container>
+        <Switch>
+          <Route exact path='/' render={()=> <h1>Home</h1>} />
+          <Route exact path='/signin' render={()=> <h1>Sign In</h1>} />
+          <Route exact path='/join' render={()=> <h1>Join</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Navbar, Nav, Form, Button, FormControl } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <div>
             <Navbar expand="md">
-        <Navbar.Brand>Fossils & Rocks</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link>Home</Nav.Link>
-                <Nav.Link>Sign In</Nav.Link>
-                <Nav.Link>Join</Nav.Link>
+                <NavLink to='/'>
+                    <Navbar.Brand>Fossils & Rocks</Navbar.Brand>
+                </NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto text-left">
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/signin'>Sign In</NavLink>
+                <NavLink to='/join'>Join</NavLink>
             </Nav>
             <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
