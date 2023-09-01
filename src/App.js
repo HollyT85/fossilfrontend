9948ae64d47da54d646be8ca0e45ff48abce6fe7
HollyT85 <h.e.turner@gmail.com>
2023-09-01 20:44:04 +0000

    signin form brought over

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import { Switch, Route } from 'react-router-dom';
 import './api/axiosDefaults'
 import JoinForm from './pages/auth/JoinForm';
+import SignInForm from './pages/auth/SignInForm';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Container>
         <Switch>
           <Route exact path='/' render={()=> <h1>Home</h1>} />
-          <Route exact path='/signin' render={()=> <h1>Sign In</h1>} />
+          <Route exact path='/signin' render={()=> <SignInForm />} />
           <Route exact path='/join' render={()=> <JoinForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import axios from "axios"
+
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+
+import { Link, useHistory } from "react-router-dom";
+
+
+// import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+// import { useRedirect } from "../../hooks/useRedirect";
+// import { setTokenTimestamp } from "../../utils/utils";
+
+// import styles from '../../styles/JoinUsSignInForms.module.css'
+
+function SignInForm() {
+
+    // const setCurrentUser = useSetCurrentUser();
+    // useRedirect('loggedIn')
+
+    const [signInData, setSignInData] = useState({
+        username: "",
+        password: "",
+    });
+    const { username, password } = signInData;
+
+    const [errors, setErrors] = useState({});
+
+    const history = useHistory();
+    const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+        const {data} = await axios.post("/dj-rest-auth/login/", signInData);
+        // setCurrentUser(data.user)
+        // setTokenTimestamp(data)
+        history.goBack()
+    } catch (err) {
+        console.log(err)
+        setErrors(err.response?.data);
+    }
+    };
+    const handleChange = (event) => {
+    setSignInData({
+        ...signInData,
+        [event.target.name]: event.target.value,
+    });
+    };
+    return (
+    <Row>
+        <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container className={`p-4 `}>
+        <h1>Sign In</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="username">
+                    <Form.Label className="d-none">Username</Form.Label>
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Username"
+                        name="username" 
+                        value={username}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+                {errors.username?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+                ))}
+
+                <Form.Group controlId="password">
+                    <Form.Label className="d-none">Password</Form.Label>
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password" 
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+                {errors.password?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+                ))}
+
+                <Button type="submit">
+                    Sign In
+                </Button>
+                {errors.non_field_errors?.map((message, idx) => (
+                <Alert key={idx} variant="warning" className="mt-3">
+                    {message}
+                </Alert>
+                ))}
+            </Form>
+        </Container>
+        <Container className={`mt-3`}>
+            <p>
+                Don't have an account? <span><Link to="/join">Join Now</Link></span> 
+            </p>
+        </Container>
+        </Col>
+        <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2`}
+        >
+        <Image
+            src={"https://res.cloudinary.com/dgggigcvd/image/upload/v1692128577/ammonite_xtsflo.png"}
+        />
+        </Col>
+    </Row>
+    );
+}
+
+export default SignInForm;
