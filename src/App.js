232104ff03210844ea0feb9924e7ebec43050dbf
HollyT85 <h.e.turner@gmail.com>
2023-09-01 17:37:54 +0000

    installed react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
